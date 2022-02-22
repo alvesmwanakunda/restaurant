db20e627d1f8774a786e29bfff1fc03497aad6da
alvesmwanakunda <thecal4@gmail.com>
2022-02-22 11:12:46 +0000

    envitonment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  BASE_API_URL: "http://localhost:5000"
-  //BASE_API_URL: "https://restaurant-app-iota.vercel.app"
+  //BASE_API_URL: "http://localhost:5000"
+  BASE_API_URL: "https://restaurant-app-iota.vercel.app"
 
 };
 
