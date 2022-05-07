7d6f8565bf277eb606a285637c79a3b7ccfcec04
alvesmwanakunda <thecal4@gmail.com>
2022-05-07 16:40:34 +0000

    New Task

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -132,9 +132,7 @@ export class SignupComponent implements OnInit {
           "^((\\+\\d{1,3}(-| )?\\(?\\d\\)?(-| )?\\d{1,5})|(\\(?\\d{2,6}\\)?))(-| )?(\\d{3,4})(-| )?(\\d{4})(( x| ext)\\d{1,5}){0,1}$|(\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,6})+)"
         )
       ]),
-      /*phone: new FormControl("",[
-        Validators.required,
-      ]),*/
+      
       categorie:new FormControl("",[Validators.required]),
       entreprise:new FormControl("",[Validators.required]),
       password:password,
